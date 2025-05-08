bb69aa282cdd2771ef80b4233a737c9296a95406
xiomararomero98 <xio.romero@duocuc.cl>
2025-05-07 20:52:20 -0400

    1er commit
    puse el control de usuario en el repositorio local

---
+++ Control de usuarios/Control-de-usuarios/src/main/java/com/example/Control/de/usuarios/ControlDeUsuariosApplication.java
@@ -0,0 +1,13 @@
+package com.example.Control.de.usuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlDeUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlDeUsuariosApplication.class, args);
+	}
+
+}
